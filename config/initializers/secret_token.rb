c67942483e72e6bf66ba4a867295541fd30a8f4f
JulissaJ <julissa.jansen@gmail.com>
2014-04-12 11:00:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TStories::Application.config.secret_key_base = 'b0f8d88317cc6dc74b438820cf5c137768406ea6460d8ac8449da18ca25883edf51cafa120d327ddc9077fabe65944396b66bea6418ba2517887eee2a8252120'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TStories::Application.config.session_store :cookie_store, key: '_T_Stories_session'
