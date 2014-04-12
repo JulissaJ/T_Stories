9983fad41a8563887ba9239a429e346be7b57ddd
JulissaJ <julissa.jansen@gmail.com>
2014-04-12 15:28:52 -0400

    user can successfully sign out"

---
+++ spec/features/user_signs_out_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'user signs out' , %Q{
+  As a signed in user
+  I want to sign out
+  so weird people can't impersonate me
+  } do
+
+  context 'as an authenticated user' do
+
+    let(:user){ FactoryGirl.create(:user) }
+
+    before :each do
+      sign_in_as(user)
+    end
+
+    scenario 'User signs out' do
+
+      click_link "Sign Out"
+      expect(page).to have_content("Sign In")
+
+    end
+  end
+end
