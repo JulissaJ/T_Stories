9f4f86c05d5dfcf30966e76043071882f6079b48
JulissaJ <julissa.jansen@gmail.com>
2014-04-12 13:03:51 -0400

    added user migation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 TStories::Application.routes.draw do
+  devise_for :users
+  root 'posts#index'
   resources :posts, only: [:new, :create, :show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
