f6d9d1ac8cc6de7ce7cbc39a027f195ecd159548
JulissaJ <julissa.jansen@gmail.com>
2014-04-12 12:01:13 -0400

    made tests for post pass

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to '/posts', notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+
+end
+
